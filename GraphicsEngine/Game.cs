73bcda536a16ca51a88e02c2774b3a5b430e4021
Misha675 <dybkovmisha9@gmail.com>
2025-05-05 18:33:08 +0300

    Version 0.0.1

---
+++ GraphicsEngine/Game.cs
@@ -0,0 +1,108 @@
+﻿using System.Reflection;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+public class Game :GameWindow
+{
+    double DeltaTime = 0;
+    public Vector3 PlayerPosition = new Vector3(0.0f, 0.0f, -5.0f);
+    public const float PlayerSpeed = 0.5f;
+    ObjLoader monkey = new ObjLoader();
+
+    public Game(GameWindowSettings gameWindowSettings,NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings,nativeWindowSettings) { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.MatrixMode(MatrixMode.Projection);
+        GL.LoadIdentity();
+        Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45f), Size.X / (float)Size.Y, 0.1f, 100f);
+        GL.LoadMatrix(ref perspective);
+
+        GL.Enable(EnableCap.DepthTest);
+        GL.Enable(EnableCap.Lighting);
+        GL.Enable(EnableCap.Light0);
+
+        GL.ClearColor(Color4.Black);
+
+        monkey.Load(@"E:\C#Sources\GraphicsEngine\GraphicsEngine\Models\monkey.obj"); //Your path to source file
+
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        GL.Translate(PlayerPosition);
+
+        GL.MatrixMode(MatrixMode.Modelview);
+
+        GL.Rotate(-(50 * DeltaTime), 0.0f, 1.0f, 0.0f);
+        //GL.Translate(-5.0f, 5.0f, -7.0f);
+        monkey.DrawModel(monkey);
+        GL.LoadIdentity();
+
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+        DeltaTime += args.Time;
+
+        if (KeyboardState.IsKeyPressed(Keys.Escape)) 
+        {
+            Close();
+        }
+        if (KeyboardState.IsKeyDown(Keys.S))
+        {
+            PlayerPosition.Z -= PlayerSpeed;
+        }
+        if (KeyboardState.IsKeyDown(Keys.W))
+        {
+            PlayerPosition.Z += PlayerSpeed;
+        }
+        if (KeyboardState.IsKeyDown(Keys.A))
+        {
+            PlayerPosition.X += PlayerSpeed;
+        }
+        if (KeyboardState.IsKeyDown(Keys.D))
+        {
+            PlayerPosition.X -= PlayerSpeed;
+        }
+        if (KeyboardState.IsKeyDown(Keys.Z))
+        {
+            PlayerPosition.Y += PlayerSpeed;
+        }
+        if (KeyboardState.IsKeyDown(Keys.X))
+        {
+            PlayerPosition.Y -= PlayerSpeed;
+        }
+    }
+
+    private static void Main() 
+    {
+        NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+        {
+            APIVersion = new Version(3, 3),
+            API = ContextAPI.OpenGL,
+            Flags = ContextFlags.Default,
+            Size = (1024,720),
+            Title = "Graphics Engine",
+            WindowBorder = WindowBorder.Fixed,
+            Profile = ContextProfile.Compatability,
+            Vsync = VSyncMode.On
+        };
+
+        using (Game game = new Game(GameWindowSettings.Default,nativeWindowSettings)) 
+        {
+            game.Run();
+        }
+    }
+}
---
+++ GraphicsEngine/Scripts/ObjLoader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+public class ObjLoader
+{
+    public List<Vector3> Vertices { get; private set; } = new();
+    public List<Vector3> Normals { get; private set; } = new();
+    public List<Face> Faces { get; private set; } = new();
+
+    public void Load(string path)
+    {
+        foreach (var line in File.ReadAllLines(path))
+        {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0) continue;
+
+            switch (parts[0])
+            {
+                case "v":
+                    Vertices.Add(ParseVec3(parts));
+                    break;
+                case "vn":
+                    Normals.Add(ParseVec3(parts));
+                    break;
+                case "f":
+                    Faces.Add(new Face(parts[1], parts[2], parts[3]));
+                    break;
+            }
+        }
+    }
+
+    private Vector3 ParseVec3(string[] parts)
+    {
+        return new Vector3(
+            float.Parse(parts[1], CultureInfo.InvariantCulture),
+            float.Parse(parts[2], CultureInfo.InvariantCulture),
+            float.Parse(parts[3], CultureInfo.InvariantCulture)
+        );
+    }
+
+    public class Face
+    {
+        public (int v, int n)[] Indices = new (int, int)[3];
+
+        public Face(string p1, string p2, string p3)
+        {
+            Indices[0] = ParseVertex(p1);
+            Indices[1] = ParseVertex(p2);
+            Indices[2] = ParseVertex(p3);
+        }
+
+        private (int v, int n) ParseVertex(string part)
+        {
+            var elems = part.Split("//"); // формат v//n
+            return (int.Parse(elems[0]) - 1, int.Parse(elems[1]) - 1);
+        }
+    }
+
+    public void DrawModel(ObjLoader model)
+    {
+        GL.Begin(PrimitiveType.Triangles);
+
+        foreach (var face in model.Faces)
+        {
+            foreach (var (vIdx, nIdx) in face.Indices)
+            {
+                var normal = model.Normals[nIdx];
+                var vertex = model.Vertices[vIdx];
+                GL.Normal3(normal);
+                GL.Vertex3(vertex);
+            }
+        }
+
+        GL.End();
+    }
+}
